dc719325a3e5bf540a0a6d7b32de76c51b617475
Kirk Pig <kriklomark@outlook.com>
2020-03-23 14:01:02 +0700

    edited TeamSet

--- src/team/TeamSet.java
+++ src/team/TeamSet.java
@@ -2,13 +2,17 @@ package team;
 
 import unit.Attacker;
 import unit.BoxToBox;
+import unit.Defender;
 
 public class TeamSet {
 	
 	static Attacker liverpoolAttacker1 = new Attacker("Sadio Mane");
 	static Attacker liverpoolAttacker2 = new Attacker("Alex Oxlade-Chamberlane");
 	static Attacker liverpoolAttacker3 = new Attacker("Mohammed Salah");
-	static BoxToBox liverpoolBoxToBox1 = new BoxToBox("");
+	static BoxToBox liverpoolBoxToBox1 = new BoxToBox("Georginio Wijnaldum");
+	static BoxToBox liverpoolBoxToBox2 = new BoxToBox("Adam Lallana");
+	static BoxToBox liverpoolBoxToBox3 = new BoxToBox("Fabinho");
+	static Defender liverpoolDefender1 = new Defender("");
 	
 
 }
