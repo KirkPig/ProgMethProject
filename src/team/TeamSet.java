003b1d2d3d91736e35e5c8db4c7f6badd19b1fd5
6231307221 <ton_jirawat@live.com>
2020-03-23 16:52:54 +0700

    add setTeam

--- src/team/TeamSet.java
+++ src/team/TeamSet.java
@@ -3,6 +3,9 @@ package team;
 import unit.Attacker;
 import unit.BoxToBox;
 import unit.Defender;
+import unit.Goalkeeper;
+import unit.God;
+import unit.Playmaker;
 
 public class TeamSet {
 	
@@ -12,7 +15,53 @@ public class TeamSet {
 	static BoxToBox liverpoolBoxToBox1 = new BoxToBox("Georginio Wijnaldum");
 	static BoxToBox liverpoolBoxToBox2 = new BoxToBox("Adam Lallana");
 	static BoxToBox liverpoolBoxToBox3 = new BoxToBox("Fabinho");
-	static Defender liverpoolDefender1 = new Defender("");
+	static Defender liverpoolDefender1 = new Defender("Dejan Lovren");
+	static Defender liverpoolDefender2 = new Defender("Virgil Van Dijk");
+	static Playmaker liverpoolPlaymaker = new Playmaker("Roberto Firmino");
+	static Goalkeeper liverpoolGoalkeeper1 = new Goalkeeper("Alisson Becker");
+	static Goalkeeper liverpoolGoalkeeper2 = new Goalkeeper("Adrian");
+	static God liverpoolGod = new God("Divock Origi");
 	
+	
+	static Attacker manUnitedAttacker1 = new Attacker("Daniel James");
+	static Attacker manUnitedAttacker2 = new Attacker("Anthony Martial");
+	static Attacker manUnitedAttacker3 = new Attacker("Marcus Rashford");
+	static BoxToBox manUnitedBoxToBox1 = new BoxToBox("Frederico Rodrigues");
+	static BoxToBox manUnitedBoxToBox2 = new BoxToBox("Paul Pogba");
+	static BoxToBox manUnitedBoxToBox3 = new BoxToBox("Scott Mctominay");
+	static Defender manUnitedDefender1 = new Defender("Victor Lindelof");
+	static Defender manUnitedDefender2 = new Defender("Phil Jones");
+	static Playmaker manUnitedPlaymaker = new Playmaker("Bruno Fernandes");
+	static Goalkeeper manUnitedGoalkeeper1 = new Goalkeeper("Sergio Romero");
+	static Goalkeeper manUnitedGoalkeeper2 = new Goalkeeper("David De Gea");
+	static God manUnitedGod = new God("Jesse Lingard");
+	
+	
+	static Attacker manCityAttacker1 = new Attacker("Sergio Aguero");
+	static Attacker manCityAttacker2 = new Attacker("Riyad Mahrez");
+	static Attacker manCityAttacker3 = new Attacker("Leroy Sane");
+	static BoxToBox manCityBoxToBox1 = new BoxToBox("Fernandinho");
+	static BoxToBox manCityBoxToBox2 = new BoxToBox("Ilkay Gundogan");
+	static BoxToBox manCityBoxToBox3 = new BoxToBox("Bernado Silva");
+	static Defender manCityDefender1 = new Defender("Nicolas Otamendi");
+	static Defender manCityDefender2 = new Defender("Aymeric Laporte");
+	static Playmaker manCityPlaymaker = new Playmaker("Kevin De Bruyne");
+	static Goalkeeper manCityGoalkeeper1 = new Goalkeeper("Ederson");
+	static Goalkeeper manCityGoalkeeper2 = new Goalkeeper("Claudio Bravo");
+	static God manCityGod = new God("Raheam Sterling");
+	
+	
+	static Attacker spurAttacker1 = new Attacker("Lucas Moura");
+	static Attacker spurAttacker2 = new Attacker("Steven Bergwijn");
+	static Attacker spurAttacker3 = new Attacker("Son Heung Min");
+	static BoxToBox spurBoxToBox1 = new BoxToBox("Erik Lamela");
+	static BoxToBox spurBoxToBox2 = new BoxToBox("Eric Dier");
+	static BoxToBox spurBoxToBox3 = new BoxToBox("Victor Wanyama");
+	static Defender spurDefender1 = new Defender("Jan Vertonghen");
+	static Defender spurDefender2 = new Defender("Toby Alderweireld");
+	static Playmaker spurPlaymaker = new Playmaker("Dele Alli");
+	static Goalkeeper spurGoalkeeper1 = new Goalkeeper("Hugo Lloris");
+	static Goalkeeper spurGoalkeeper2 = new Goalkeeper("Paulo Gazzaniga");
+	static God spurGod = new God("Moussa Sissoko");
 
 }
