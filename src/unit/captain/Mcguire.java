2c98f93f0f96faf143a85899ac81616d6a351af5
Kirk Pig <kriklomark@outlook.com>
2020-03-27 23:21:44 +0700

    finished Mcguire

--- src/unit/captain/Mcguire.java
+++ src/unit/captain/Mcguire.java
@@ -2,7 +2,9 @@ package unit.captain;
 
 import java.util.ArrayList;
 
+import logic.GameController;
 import unit.Captain;
+import unit.Empty;
 import unit.base.Unit;
 
 public class Mcguire extends Captain{
@@ -10,7 +12,22 @@ public class Mcguire extends Captain{
 	@Override
 	public ArrayList<Unit> getMoveUnit() {
 		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Unit> canMove = new ArrayList<Unit>();
+		int x = getCoordinate().getX();
+		int y = getCoordinate().getY();
+		for(Unit i: GameController.gameBoard.getAdjacentUnit(x, y)) {
+			if(i instanceof Empty) {
+				for(var j: GameController.gameBoard.getAdjacentUnit(i.getCoordinate().getX(), i.getCoordinate().getY())) {
+					if(j.getCoordinate() != this.getCoordinate()) {
+						if(!(j instanceof Empty)) {
+							canMove.add(i);
+							break;
+						}
+					}
+				}
+			}
+		}
+		return canMove;
 	}
 	
 	
