f6cfb938e7e90cb858c1ae97920bfe5807d2ada0
Kirk Pig <kriklomark@outlook.com>
2020-03-27 19:57:26 +0700

    added Mcguire(unfinished)

---
+++ src/unit/captain/Mcguire.java
@@ -0,0 +1,18 @@
+package unit.captain;
+
+import java.util.ArrayList;
+
+import unit.Captain;
+import unit.base.Unit;
+
+public class Mcguire extends Captain{
+
+	@Override
+	public ArrayList<Unit> getMoveUnit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
