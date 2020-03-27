0e3b39c8e26484f0c0e5fc041847998739beb32a
6231307221 <ton_jirawat@live.com>
2020-03-27 16:16:50 +0700

    add attackerTest

--- src/test/AttackerTest.java
+++ src/test/AttackerTest.java
@@ -1,10 +1,16 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import logic.GameController;
 import test.base.GameTest;
 import unit.Attacker;
 import unit.BoxToBox;
 import unit.Captain;
 import unit.Defender;
+import unit.Empty;
 import unit.Goalkeeper;
 import unit.God;
 import unit.base.Unit;
@@ -21,5 +27,22 @@ public class AttackerTest extends GameTest {
 		super.setUpBeforeEachTest();
 	}
 	
-	
+	void testMoveAttacker() {
+		unit1.setOwner(owner1);
+		unit2.setOwner(owner2);
+		unit3.setOwner(owner1);
+		unit4.setOwner(owner2);
+		unit5.setOwner(owner2);
+		unit6.setOwner(owner2);
+		GameController.gameBoard.addUnit(unit1, 1, 1);
+		GameController.gameBoard.addUnit(unit2, 1, 2);
+		GameController.gameBoard.addUnit(unit3, 1, 3);
+		GameController.gameBoard.addUnit(unit4, 2, 2);
+		GameController.gameBoard.addUnit(unit5, 3, 2);
+		GameController.gameBoard.addUnit(unit6, 4, 3);
+		ArrayList<Unit> answer = new ArrayList<Unit>();
+		answer.add(new Empty(1, 4));
+		answer.add(new Empty(5, 3));
+		assertEquals(answer, unit1.getMoveUnit());
+	}
 }
