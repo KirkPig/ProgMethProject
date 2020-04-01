a597bb46e5b2bbda2594f5d54d2261685531d252
Kirk Pig <kriklomark@outlook.com>
2020-04-01 20:11:06 +0700

    added Main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,14 +3,14 @@ package main;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application{
+public class Main extends Application {
 
 	@Override
-	public void start(Stage primaryStage){
+	public void start(Stage primaryStage) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
