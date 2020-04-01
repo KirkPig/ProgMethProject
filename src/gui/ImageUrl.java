111e9b1fe1839606630b6a630b5bca798876e662
Kirk Pig <kriklomark@outlook.com>
2020-04-01 21:58:17 +0700

    added Main and Cover Scene

---
+++ src/gui/ImageUrl.java
@@ -0,0 +1,16 @@
+package gui;
+
+public class ImageUrl {
+	
+	//Cover Menu
+	public static final String coverLogo = "file:res/ui/cover/title.png";
+	public static final String coverSideLogo = "file:res/ui/cover/subtitle.png";
+	public static final String coverBackground = "file:res/ui/cover/background.jpg";
+	
+	//Main Menu
+	
+	//Select Team Menu
+	
+	//Game Scene
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,16 @@
 package main;
 
+import gui.ImageUrl;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -8,11 +18,36 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		// TODO Auto-generated method stub
-
+		primaryStage.setScene(getCoverScene());
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("FIFA Hex");
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
+
+	}
+
+	public static Scene getCoverScene() {
+		AnchorPane coverSceneRoot = new AnchorPane();
+
+		coverSceneRoot.setBackground(
+				new Background(new BackgroundImage(new Image(ImageUrl.coverBackground, 1280, 720, false, false), BackgroundRepeat.REPEAT,
+						BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
+
+		ImageView coverLogo = new ImageView(new Image(ImageUrl.coverLogo));
+		coverLogo.setX(272);
+		coverLogo.setY(94);
+		coverSceneRoot.getChildren().add(coverLogo);
+		
+		ImageView coverSideLogo = new ImageView(new Image(ImageUrl.coverSideLogo));
+		coverSideLogo.setX(888);
+		coverSideLogo.setY(10);
+		coverSceneRoot.getChildren().add(coverSideLogo);
+
+		Scene coverScene = new Scene(coverSceneRoot, 1280, 720);
+		return coverScene;
 	}
 
 }
